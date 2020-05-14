0a8a87f9736c1b24bfe654f91bd0d498f77e4141
Vinicius Coelho de Abreu <viniciuscdeabreu@gmail.com>
2020-05-14 20:59:44 -0300

    Tirado sout do Utils

--- Java/src/br/com/ies/util/Utils.java
+++ Java/src/br/com/ies/util/Utils.java
@@ -63,9 +63,6 @@ public class Utils {
 
 	public static void setParameter(PreparedStatement preparedStatement, int index, Object value) {
 		String typeName = value == null ? "null" : value.getClass().getTypeName();
-
-System.out.println(index + "/" + value);
-		
 		try {
 			switch (typeName) {
 			case "java.lang.String":
