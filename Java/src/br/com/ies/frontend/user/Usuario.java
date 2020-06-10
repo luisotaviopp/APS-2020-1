bac253dff73ac7015da587b44977fa1f859b63f1
Vinicius Coelho de Abreu <49377325+ViniciusCoelhoDeAbreu@users.noreply.github.com>
2020-06-09 21:45:02 -0300

    Update Usuario.java

--- Java/src/br/com/ies/frontend/user/Usuario.java
+++ Java/src/br/com/ies/frontend/user/Usuario.java
@@ -1,32 +1,22 @@
 package br.com.ies.frontend.user;
 
-import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.BorderLayout;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.Font;
 import javax.swing.SwingConstants;
 
-import br.com.ies.frontend.Index;
+import br.com.ies.backend.Main;
+import br.com.ies.backend.util.PersistenceUtil;
+import br.com.ies.backend.util.Util;
 import br.com.ies.frontend.builder.ComponentBuilder;
-
-import java.awt.Color;
-import java.awt.Component;
-
-import javax.swing.JPasswordField;
+import br.com.ies.frontend.util.Constants;
 
 public class Usuario {
 
 	private JFrame frame;
-	private JTextField inputNome;
-	private JTextField inputEmail;
-	private JTextField inputUsuario;
-	private JPasswordField inputSenhaAtual;
-	private JPasswordField inputConfirmeSenha;
-	private JPasswordField inputNovaSenha;
 
 	/**
 	 * Launch the application.
@@ -42,6 +32,7 @@ public class Usuario {
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	@SuppressWarnings("deprecation")
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 900, 600);
@@ -49,46 +40,56 @@ public class Usuario {
 		frame.getContentPane().setLayout(null);
 
 		frame.getContentPane().add(ComponentBuilder.buildLabel("MEU PERFIL", "Franklin Gothic Medium", Font.BOLD, 20, SwingConstants.CENTER, null, null, null, 103, 20, 675, 30, null));
-		
-		// Input Nome
-		frame.getContentPane().add(ComponentBuilder.buildTextField("Franklin Gothic Medium", Font.PLAIN, 13, 313, 114, 441, 30, 10));
-
-		// Input Email
-		frame.getContentPane().add(ComponentBuilder.buildTextField("Franklin Gothic Medium", Font.PLAIN, 13, 313, 157, 441, 30, 10));
-
-		// Input Usuario
-		frame.getContentPane().add(ComponentBuilder.buildTextField("Franklin Gothic Medium", Font.PLAIN, 13, 313, 200, 441, 30, 10));
-
 		frame.getContentPane().add(ComponentBuilder.buildButton("VOLTAR", "Franklin Gothic Medium", Font.PLAIN, 13, 127,469, 300, 40, null, null,
 				() -> {
-					new Index().toggleFrame();
+					new UserDashboard().toggleFrame();
 					toggleFrame();
 				}));
-
 		frame.getContentPane().add(ComponentBuilder.buildLabel("DADOS PESSOAIS", "Franklin Gothic Medium", Font.BOLD, 13, SwingConstants.LEFT, null, null, null, 127, 78, 250, 30, null));
 		frame.getContentPane().add(ComponentBuilder.buildLabel("Nome", "Franklin Gothic Medium", Font.PLAIN, 13, null, null, null, null, 127, 121, 56, 16, null));
 		frame.getContentPane().add(ComponentBuilder.buildLabel("Email", "Franklin Gothic Medium", Font.PLAIN, 13, null, null, null, null, 127, 164, 56, 16, null));
-		frame.getContentPane().add(ComponentBuilder.buildLabel("Usuário", "Franklin Gothic Medium", Font.PLAIN, 13, null, null, null, null, 127, 207, 56, 16, null));
+		frame.getContentPane().add(ComponentBuilder.buildLabel("UsuÃ¡rio", "Franklin Gothic Medium", Font.PLAIN, 13, null, null, null, null, 127, 207, 56, 16, null));
 		frame.getContentPane().add(ComponentBuilder.buildLabel("REDEFINIR SENHA","Franklin Gothic Medium", Font.BOLD, 13, SwingConstants.LEFT, null, null, null, 127, 280, 250, 30, null));
-		frame.getContentPane().add(ComponentBuilder.buildButton("ATUALIZAR","Franklin Gothic Medium", Font.PLAIN, 13, 454, 469, 300, 40, new Color(0, 250, 154), null,
-				() -> {
-					
-				}));
 		frame.getContentPane().add(ComponentBuilder.buildLabel("Senha atual", "Franklin Gothic Medium", Font.PLAIN, 13, null, null, null, null, 127, 409, 81, 16, null));
 		frame.getContentPane().add(ComponentBuilder.buildLabel("Nova senha", "Franklin Gothic Medium", Font.PLAIN, 13, null, null, null, null,  127, 323, 73, 16, null));
 		frame.getContentPane().add(ComponentBuilder.buildLabel("Confirme a nova senha","Franklin Gothic Medium", Font.PLAIN, 13, null, null, null, null, 127, 366, 145, 16, null));
-
-		inputSenhaAtual = new JPasswordField();
-		inputSenhaAtual.setBounds(313, 402, 441, 30);
+		
+		JTextField inputNome  = ComponentBuilder.buildTextField("Franklin Gothic Medium", Font.PLAIN, 13, 313, 114, 441, 30, 10);
+		JTextField inputEmail =  ComponentBuilder.buildTextField("Franklin Gothic Medium", Font.PLAIN, 13, 313, 157, 441, 30, 10);
+		JTextField inputUsuario = ComponentBuilder.buildTextField("Franklin Gothic Medium", Font.PLAIN, 13, 313, 200, 441, 30, 10);
+		JPasswordField inputSenhaAtual = ComponentBuilder.buildPasswordField("Franklin Gothic Medium", Font.PLAIN, 13, 313, 402, 441, 30);
+		JPasswordField inputNovaSenha = ComponentBuilder.buildPasswordField("Franklin Gothic Medium", Font.PLAIN, 13, 313, 316, 441, 30);
+		JPasswordField inputConfirmeSenha = ComponentBuilder.buildPasswordField("Franklin Gothic Medium", Font.PLAIN, 13, 313, 359, 441, 30);
+		
+		
+		frame.getContentPane().add(inputNome);
+		frame.getContentPane().add(inputEmail);
+		frame.getContentPane().add(inputUsuario);
 		frame.getContentPane().add(inputSenhaAtual);
-
-		inputConfirmeSenha = new JPasswordField();
-		inputConfirmeSenha.setBounds(313, 359, 441, 30);
-		frame.getContentPane().add(inputConfirmeSenha);
-
-		inputNovaSenha = new JPasswordField();
-		inputNovaSenha.setBounds(313, 316, 441, 30);
 		frame.getContentPane().add(inputNovaSenha);
+		frame.getContentPane().add(inputConfirmeSenha);
+		
+		
+		frame.getContentPane().add(ComponentBuilder.buildButton("ATUALIZAR","Franklin Gothic Medium", Font.PLAIN, 13, 454, 469, 300, 40, new Color(0, 250, 154), null,
+				() -> {
+					if(!(inputNovaSenha.getText().equals(inputConfirmeSenha.getText())) || inputNovaSenha.getText().isEmpty()) {
+						Util.showMessage(Constants.PASSWORDS_DONT_MATCH);
+						return;
+					}
+					Main.getUserManager().getUsuario().setUsrSenha(inputNovaSenha.getText());
+					PersistenceUtil.persist(Main.getUserManager().getUsuario());
+					Util.showMessage(Constants.PERSONAL_INFO_CHANGED);
+					
+					new UserDashboard().toggleFrame();
+					toggleFrame();
+				}));
+		
+
+		inputNome.setText(Main.getUserManager().getUsuario().getUsrNome());
+		inputEmail.setText(Main.getUserManager().getUsuario().getUsrEmail());
+		inputUsuario.setText(Main.getUserManager().getUsuario().getUsrLogin());
+		inputSenhaAtual.setText(Main.getUserManager().getUsuario().getUsrSenha());
+		
 	}
 
 	public JFrame getFrame() {
