4b3aca0ce872e7dbf85459afd2910d3d458ea18a
Vinicius Coelho de Abreu <49377325+ViniciusCoelhoDeAbreu@users.noreply.github.com>
2020-06-09 21:55:32 -0300

    Update Util.java

--- Java/src/br/com/ies/backend/util/Util.java
+++ Java/src/br/com/ies/backend/util/Util.java
@@ -1,5 +1,7 @@
 package br.com.ies.backend.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringJoiner;
@@ -22,24 +24,34 @@ public class Util {
 		columnValueList.forEach(cv -> stringJoiner.add(cv.getColumn()));
 		return stringJoiner.toString();
 	}
+
 	@SuppressWarnings("unchecked")
-	public static List<Object[]> castObjectToList(Object object){
-		return (List<Object[]>)object;
+	public static List<Object[]> castObjectToList(Object object) {
+		return (List<Object[]>) object;
+	}
+
+	public static String dateToString(String format, Date date) {
+		return new SimpleDateFormat(format).format(date);
 	}
 
 	public static void showMessage(String message) {
 		JOptionPane.showMessageDialog(null, message);
 	}
-	
+
+	public static String[] listToArray(List<String> list) {
+		return list.stream().toArray(String[]::new);
+	}
+
 	public static String fillColunasAndValuesFromColumnStringList(List<String> columnStringList) {
 		StringJoiner stringJoiner = new StringJoiner(",");
 		columnStringList.forEach(cs -> stringJoiner.add(cs));
 		return stringJoiner.toString();
 	}
+
 	public static void removeColumnFromList(String column, List<ColumnValueDTO> list) {
 		Iterator<ColumnValueDTO> iterator = list.iterator();
-		while(iterator.hasNext()) {
-			if(iterator.next().getColumn().equalsIgnoreCase(column))
+		while (iterator.hasNext()) {
+			if (iterator.next().getColumn().equalsIgnoreCase(column))
 				iterator.remove();
 		}
 	}
