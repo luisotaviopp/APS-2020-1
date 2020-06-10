59a5a35794a139215ce0ed44487b936beb24894e
Vinicius Coelho de Abreu <49377325+ViniciusCoelhoDeAbreu@users.noreply.github.com>
2020-06-09 21:45:20 -0300

    Update Constants.java

--- Java/src/br/com/ies/frontend/util/Constants.java
+++ Java/src/br/com/ies/frontend/util/Constants.java
@@ -3,9 +3,12 @@ package br.com.ies.frontend.util;
 public class Constants {
 
 	
-	public static final String USER_ALREADY_EXISTS = new String("J· existe um usu·rio cadastrado com esse login.");
-	public static final String PASSWORDS_DONT_MATCH = new String("As senhas informadas n„o s„o as mesmas.");
-	public static final String USER_REGISTRED = new String("Usu·rio registrado com sucesso!");
-	public static final String USER_DOESNT_EXIST = new String("Usu·rio com esse login n„o existe.");
-	public static final String INVALID_PASSWORD = new String("Senha informada È inv·lida.");
+	public static final String USER_ALREADY_EXISTS = new String("J√° existe um usu√°rio cadastrado com esse login.");
+	public static final String PASSWORDS_DONT_MATCH = new String("As senhas informadas n√£o s√£o as mesmas.");
+	public static final String USER_REGISTRED = new String("Usu√°rio registrado com sucesso!");
+	public static final String USER_DOESNT_EXIST = new String("Usu√°rio com esse login n√£o existe.");
+	public static final String INVALID_PASSWORD = new String("Senha informada √© inv√°lida.");
+	public static final String PERSONAL_INFO_CHANGED = new String("Dados alterados com sucesso!");
+	public static final String NEED_TO_FILL_ALL_FIELDS = new String("Ao utilizar a forma de pagamento cart√£o de cr√©dito/d√©bito, preencha todos os dados correspondentes.");
+	public static final String SUCCESSFUL_BUY = new String("Ingresso(s) comprado(s) com sucesso!");
 }
