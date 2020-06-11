7a1ec5d1690ead79cca9687d3212aec23458171e
Vinicius Coelho de Abreu <49377325+ViniciusCoelhoDeAbreu@users.noreply.github.com>
2020-06-11 12:01:10 -0300

    Update Constants.java

--- Java/src/br/com/ies/frontend/util/Constants.java
+++ Java/src/br/com/ies/frontend/util/Constants.java
@@ -6,7 +6,10 @@ public class Constants {
 	public static final String USER_ALREADY_EXISTS = new String("Já existe um usuário cadastrado com esse login.");
 	public static final String PASSWORDS_DONT_MATCH = new String("As senhas informadas não são as mesmas.");
 	public static final String USER_REGISTRED = new String("Usuário registrado com sucesso!");
+	public static final String USER_CHANGED = new String("Usuário atualizado com sucesso!");
+	public static final String USER_DISABLED = new String("Usuário desativado com sucesso!");
 	public static final String USER_DOESNT_EXIST = new String("Usuário com esse login não existe.");
+	public static final String USER_DISABLED_2 = new String("Usuário desativado.");
 	public static final String INVALID_PASSWORD = new String("Senha informada é inválida.");
 	public static final String PERSONAL_INFO_CHANGED = new String("Dados alterados com sucesso!");
 	public static final String NEED_TO_FILL_ALL_FIELDS = new String("Ao utilizar a forma de pagamento cartão de crédito/débito, preencha todos os dados correspondentes.");
