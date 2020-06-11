11550df26d9e397ce5d47b01bccafcc49e65aff9
joaoheing <50169829+joaoheing@users.noreply.github.com>
2020-06-11 18:01:08 -0300

    Update Artistas.java

--- Java/src/br/com/ies/frontend/administration/Artistas.java
+++ Java/src/br/com/ies/frontend/administration/Artistas.java
@@ -1,26 +1,30 @@
 package br.com.ies.frontend.administration;
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.StringJoiner;
 
-import javax.swing.AbstractListModel;
-import javax.swing.JButton;
+import javax.swing.DefaultListModel;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 
+import br.com.ies.backend.Main;
+import br.com.ies.backend.dto.PersistenceParameterDTO;
+import br.com.ies.backend.entity.ArtistaEntity;
+import br.com.ies.backend.type.PersistenceType;
+import br.com.ies.backend.util.PersistenceUtil;
+import br.com.ies.backend.util.Util;
 import br.com.ies.frontend.builder.ComponentBuilder;
+import br.com.ies.frontend.util.Constants;
 
 public class Artistas {
 
 	private JFrame frame;
-	private JTextField inputNome;
 
 	/**
 	 * Launch the application.
@@ -42,51 +46,99 @@ public class Artistas {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JList listaArtistas = new JList();
-		listaArtistas.setBackground(Color.LIGHT_GRAY);
-		listaArtistas.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 16));
-		listaArtistas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		listaArtistas.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Artista 01", "Artista 02", "Artista 03", "Artista 04", "Artista 05", "Artista 06"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
+		DefaultListModel<Object> defaultList = new DefaultListModel<>();
+		PersistenceParameterDTO<String> persistenceParameterDTO2 = new PersistenceParameterDTO<>();
+		persistenceParameterDTO2.setParameter("SELECT art_codigo,art_nome FROM evento.artista");
+		
+		Main.getPersistenceManager().getPersistance(PersistenceType.POSTGRES).select(persistenceParameterDTO2,
+				(u) ->{
+				List<Object[]>	list = Util.castObjectToList(u);
+				for(Object[] object : list) {
+					StringJoiner stringJoiner = new StringJoiner(" - "); 
+				
+					Integer codigo = (Integer) object[0];
+					String nome = (String) object[1];
+					
+					stringJoiner.add(codigo.toString());
+					stringJoiner.add(nome);
+					defaultList.addElement(stringJoiner.toString());
+					
+				}
 		});
+		
+		JList<Object> listaArtistas = ComponentBuilder.buildList("Franklin Gothic Medium", Font.PLAIN, 16, Color.LIGHT_GRAY, 29, 101, 258, 251, ListSelectionModel.SINGLE_SELECTION, defaultList);
+		frame.getContentPane().add(listaArtistas);
+		
 		listaArtistas.setBounds(54, 101, 258, 251);
 		frame.getContentPane().add(listaArtistas);
 		
 		frame.getContentPane().add(ComponentBuilder.buildLabel("ARTISTAS", "Franklin Gothic Medium", Font.BOLD, 20, SwingConstants.CENTER, null, null, null, 0, 20, 882, 30, null));
 		frame.getContentPane().add(ComponentBuilder.buildButton("DELETAR ARTISTA", "Franklin Gothic Medium", Font.PLAIN, 13, 54, 365, 255, 40, null, null,
 				 () -> {
-						
-					}));
-		frame.getContentPane().add(ComponentBuilder.buildButton("EDITAR ARTISTA", "Franklin Gothic Medium", Font.PLAIN, 13, 54, 424, 255, 40, null, null,
-				 () -> {
-						
+					 if(listaArtistas.getSelectedValue() ==null)return;
+						Integer id = Integer.valueOf(listaArtistas.getSelectedValue().toString().split(" - ")[0]);
+						try {
+						PreparedStatement preparedStatement = Main.getConnectionFactory().getPreparedStatement(String.format("DELETE FROM evento.artista WHERE art_codigo = '%s'", id));
+						preparedStatement.execute();
+						preparedStatement.close();
+						new Artistas().toggleFrame();
+						toggleFrame();
+						}catch(Exception e) {
+							e.printStackTrace();
+						}	
 					}));
 		
 		frame.getContentPane().add(ComponentBuilder.buildButton("VOLTAR", "Franklin Gothic Medium", Font.PLAIN, 13, 54, 479, 255, 40, null, null,
 				 () -> {
-						
+						new AdministratorDashboard().toggleFrame();
+						toggleFrame();
 					}));
-		frame.getContentPane().add(ComponentBuilder.buildButton("CRIAR ARTISTA", "Franklin Gothic Medium", Font.PLAIN, 13, 332, 479, 515, 40, new Color(0, 250, 154), null,
-				 () -> {
-						
-					}));
-		frame.getContentPane().add(ComponentBuilder.buildLabel("SELECIONE OU CRIE UM ARTISTA", "Franklin Gothic Medium", Font.PLAIN, 13, SwingConstants.CENTER, null, null, null, 54, 79, 258, 16, null));
-		// input nome
-		frame.getContentPane().add(ComponentBuilder.buildTextField("Franklin Gothic Medium", Font.PLAIN, 13, 332, 101, 515, 40, 10));
-		
-		JEditorPane txtBoxDescrição = new JEditorPane();
-		txtBoxDescrição.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 13));
-		txtBoxDescrição.setBounds(332, 188, 515, 276);
-		frame.getContentPane().add(txtBoxDescrição);
 		
 		frame.getContentPane().add(ComponentBuilder.buildLabel("NOME", "Franklin Gothic Medium", Font.PLAIN, 13, SwingConstants.CENTER, null, null, null, 460, 79, 258, 16, null));
-		frame.getContentPane().add(ComponentBuilder.buildLabel("DESCRIÇÃO", "Franklin Gothic Medium", Font.PLAIN, 13, SwingConstants.CENTER, null, null, null, 460, 159, 258, 16, null));
+		frame.getContentPane().add(ComponentBuilder.buildLabel("DESCRIÃ‡ÃƒO", "Franklin Gothic Medium", Font.PLAIN, 13, SwingConstants.CENTER, null, null, null, 460, 159, 258, 16, null));
+		JTextField inputNome = ComponentBuilder.buildTextField("Franklin Gothic Medium", Font.PLAIN, 13, 332, 101, 515, 40, 10);
+
+		frame.getContentPane().add(inputNome);		
+		
+		JEditorPane txtBoxDescricao = new JEditorPane();
+		txtBoxDescricao.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 13));
+		txtBoxDescricao.setBounds(332, 188, 515, 276);
+		frame.getContentPane().add(txtBoxDescricao);
+		
+		frame.getContentPane().add(ComponentBuilder.buildButton("CRIAR ARTISTA", "Franklin Gothic Medium", Font.PLAIN, 13, 332, 479, 515, 40, new Color(0, 250, 154), null,
+				 () -> {
+					 ArtistaEntity artista = new ArtistaEntity();
+					 if(listaArtistas.getSelectedValue() != null)
+						 artista.setArtCodigo(Integer.valueOf(Integer.valueOf(listaArtistas.getSelectedValue().toString().split(" - ")[0])));
+					 	artista.setArtNome(inputNome.getText());
+					 	artista.setArtDescricao(txtBoxDescricao.getText());
+
+						PersistenceUtil.persist(artista);
+						Util.showMessage(Constants.ARTISTAS_REGISTRED);
+						new Artistas().toggleFrame();
+						toggleFrame();
+					}));
+		
+		frame.getContentPane().add(ComponentBuilder.buildLabel("SELECIONE OU CRIE UM ARTISTA", "Franklin Gothic Medium", Font.PLAIN, 13, SwingConstants.CENTER, null, null, null, 54, 79, 258, 16, null));
+		
+		frame.getContentPane().add(ComponentBuilder.buildButton("EDITAR ARTISTA", "Franklin Gothic Medium", Font.PLAIN, 13, 54, 424, 255, 40, null, null,
+				 () -> {
+						if(listaArtistas.getSelectedValue() ==null)return;
+						Integer id = Integer.valueOf(listaArtistas.getSelectedValue().toString().split(" - ")[0]);
+						
+						PersistenceParameterDTO<String> select1 = new PersistenceParameterDTO<>();
+						select1.setParameter(String.format("SELECT art_codigo,art_nome,art_descricao FROM evento.artista WHERE art_codigo = '%s' " ,id));
+						
+						Main.getPersistenceManager().getPersistance(PersistenceType.POSTGRES).select(select1,
+								(u) ->{
+									List<Object[]> list = Util.castObjectToList(u);
+									Object[] object = list.get(0);
+									
+									inputNome.setText(String.valueOf(object[1]));
+									txtBoxDescricao.setText(String.valueOf(object[2]));
+
+								});
+					}));
 	}
 
 	public JFrame getFrame() {
