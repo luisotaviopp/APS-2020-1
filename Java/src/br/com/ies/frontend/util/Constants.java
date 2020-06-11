b91674317350fb59a664a0f9de020fbe1dcbe0a6
joaoheing <50169829+joaoheing@users.noreply.github.com>
2020-06-11 18:02:40 -0300

    Update Constants.java

--- Java/src/br/com/ies/frontend/util/Constants.java
+++ Java/src/br/com/ies/frontend/util/Constants.java
@@ -3,15 +3,20 @@ package br.com.ies.frontend.util;
 public class Constants {
 
 	
-	public static final String USER_ALREADY_EXISTS = new String("Já existe um usuário cadastrado com esse login.");
-	public static final String PASSWORDS_DONT_MATCH = new String("As senhas informadas não são as mesmas.");
-	public static final String USER_REGISTRED = new String("Usuário registrado com sucesso!");
-	public static final String USER_CHANGED = new String("Usuário atualizado com sucesso!");
-	public static final String USER_DISABLED = new String("Usuário desativado com sucesso!");
-	public static final String USER_DOESNT_EXIST = new String("Usuário com esse login não existe.");
-	public static final String USER_DISABLED_2 = new String("Usuário desativado.");
-	public static final String INVALID_PASSWORD = new String("Senha informada é inválida.");
+	public static final String USER_ALREADY_EXISTS = new String("Ja existe um usuario cadastrado com esse login.");
+	public static final String PASSWORDS_DONT_MATCH = new String("As senhas informadas nao sao as mesmas.");
+	public static final String USER_REGISTRED = new String("Usuario registrado com sucesso!");
+	public static final String USER_CHANGED = new String("Usuario atualizado com sucesso!");
+	public static final String USER_DISABLED = new String("Usuario desativado com sucesso!");
+	public static final String USER_DOESNT_EXIST = new String("Usuario com esse login nao existe.");
+	public static final String USER_DISABLED_2 = new String("Usuario desativado.");
+	public static final String INVALID_PASSWORD = new String("Senha informada invalida.");
 	public static final String PERSONAL_INFO_CHANGED = new String("Dados alterados com sucesso!");
-	public static final String NEED_TO_FILL_ALL_FIELDS = new String("Ao utilizar a forma de pagamento cartão de crédito/débito, preencha todos os dados correspondentes.");
+	public static final String NEED_TO_FILL_ALL_FIELDS = new String("Ao utilizar a forma de pagamento cartao de credito/debito, preencha todos os dados correspondentes.");
 	public static final String SUCCESSFUL_BUY = new String("Ingresso(s) comprado(s) com sucesso!");
+	public static final String EVENT_CHANGED = new String("Evento criado/atualizado com sucesso!");
+	public static final String EVENT_DELETED = new String("Evento deletado com sucesso!");
+	public static final String LOCAL_REGISTRED = new String("Local cadastrado com sucesso!");
+	public static final String ARTISTAS_REGISTRED = new String("Artistas registrado com sucesso!");
+	
 }
